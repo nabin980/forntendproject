1f958021f9030464f3bfba095ec3252d9fa4c3cb
Shenrt <olinaveen98@gmail.com>
2024-04-08 00:42:03 +0545

    animation done but not perfect

--- src/components/aboutus/About_us.jsx
+++ src/components/aboutus/About_us.jsx
@@ -33,35 +33,18 @@ const About_us = () => {
           <br /> for the Modern Woman
         </p>
       </div>
-      <div className="image_section">
-        <div className="image-item1">
-          <img src={image1} alt="" />
-        </div>
-        <div className="image-item3">
-          <img src={image3} alt="" />
-        </div>
-        <div className="image-item4">
-          <img src={image4} alt="" />
-        </div>
-        <div className="image-item2">
-          <img src={image2} alt="" />
-        </div>
-        <div className="image-item5">
-          <img src={image5} alt="" />
-        </div>
-        <div className="horizontal">
-          <div className="image-item6">
-            <img src={image6} alt="" />
-          </div>
-          <div className="image-item7">
-            <img src={image7} alt="" />
-          </div>
-          <div className="image-item8">
-            <img src={image8} alt="" />
-          </div>
-        </div>
+      <div class="grid_container">
+        <div class="item1"><img src={image2} alt=""/></div>
+        <div class="item2"><img src={image3} alt=""/></div>
+        <div class="item3"><img src={image4} alt=""/></div>
+        <div class="item4"><img src={image5} alt=""/></div>
+      <div class="horizontal">
+        <div class="item5"><img src={image6} alt=""/></div>
+        <div class="item6"><img src={image7} alt=""/></div>
+        <div class="item8"><img src={image8} alt=""/></div>
       </div>
-
+        <div class="item9"><img src={image1} alt=""/></div>
+    </div>
       <div className="ourstory">
         <p className="our_story">
           Our&nbsp;<span className="story">Story</span>
--- src/components/animation/Animation.jsx
+++ src/components/animation/Animation.jsx
@@ -1,58 +1,106 @@
-import React from 'react'
-import './Animation.css'
-import background from '../../assets/banner.jpg'
-import appstore from '../../assets/Apple.png'
-import round from '../../assets/round.png'
-import googleplay from '../../assets/Google.png'
-
+import React from "react";
+import "./Animation.css";
+import background from "../../assets/banner.jpg";
+import image1 from "../../assets/b1.png";
+import image2 from "../../assets/b2.png";
+import image3 from "../../assets/b3.png";
+import image4 from "../../assets/b4.png";
+import appstore from "../../assets/Apple.png";
+import round from "../../assets/round.png";
+import googleplay from "../../assets/Google.png";
 
 const Animation = () => {
   return (
     <>
-    <div className="container">
-      <div className="rotation">
-            <img className='rotating_animation' src={round} alt="" />
-      </div>
-      <div className="background">
-        <div className="overlay">
-          <img src={background} alt="image" />
+      <div className="container">
+        <div className="rotation">
+          <img className="rotating_animation" src={round} alt="" />
         </div>
-        <div className='top t1'><span className='experience'>Experience</span> the Future of Female Commerce <br />
-        with <span className='cnex'>Cnex</span> </div>
-        <div className='top t2'>Where <span className='experience'> Fashion</span> meets Femininity </div>
-        <div className='top t3'>Radiate <span className="experience">Confidence,</span> <br />Radiate Style</div>
-        <div className='top t4'><span className="experience">Fashion,</span> redefine.</div>
+        <div className="background">
+          <div className="overlay">
+            {/* <img src={background} alt="image" /> */}
+            <div className="bot b1">
+              <img src={image1} alt="" className="bimg" />
+            </div>
+            <div className="bot b2">
+              <img src={image2} alt="" className="bimg" />
+            </div>
+            <div className="bot b3">
+              <img src={image3} alt="" className="bimg" />
+            </div>
+            <div className="bot b4">
+              <img src={image4} alt="" className="bimg" />
+            </div>
+          </div>
+          <div class="text-wrapper">
+            <div class="top t1">
+              <span className="experience">
+                Experience
+              </span>{" "}
+              the Future of
+              <br />
+              Female Commerce
+              <br />
+              with
+              <span className="experience">
+                Cnex
+              </span>
+              <p>
+                Order, Preorder, and Book - Your Ultimate Destination for Female
+                Products and Services
+              </p>
+            </div>
 
-        <div className='down d1'>Order, Preorder, and Book - Your Ultimate Destination for Female Products and Services</div>
-        <div className='down d2'>Celebrate the essence of womanhood in every styling detail.</div>
-        <div className='down d3'>Look and feel best in every moment with chic fashion pieces</div>
-        <div className="download">
-          <a href="#appstore">
-            <img src={appstore} alt="" />
-          </a>
-          <a href="#goolgeplay">
-            <img src={googleplay} alt="" />
-          </a>
-        </div>  
+            <div class="top t2">
+              Where <span className="experience">Fashion </span> meets
+              <br />
+              Feminity
+              <p>Celebrate the essence of womanhood in every styling detail</p>
+            </div>
+
+            <div class="top t3">
+              Radiate{" "}
+              <span className="experience">
+                Confidience,
+              </span>
+              <br />
+              Radiate Style
+              <p>Look and feel best in every moment with chic fashion pieces</p>
+            </div>
+
+            <div class="top t4">
+              <span className="experience">
+                Fashion,
+              </span>
+              <span className="experience"> redefined.</span>
+              <p>
+                Order, Preorder, and Book - Your Ultimate Destination for Female
+                Products and Services
+              </p>
+            </div>
+          </div>
+          <div class="image">
+        <img src={appstore} alt="" className="images"/>
+        <img src={googleplay} alt="" className="images"/>
       </div>
-      
-    </div>
-    <div className="numbers">
-    <div className="clientsnumber">
-      <a href="">200+</a>
-      <a href="">100+</a>
-      <a href="">99K+</a>
-      <a href="">24H</a>
-    </div>
-    <div className="clients">
-      <p>Clients Worldwide</p>
-      <p>Clients Worldwide</p>
-      <p>Clients Worldwide</p>
-      <p>Clients Worldwide</p>
-    </div>
-  </div>
-  </>
-  )
-}
+        </div>
+      </div>
+      <div className="numbers">
+        <div className="clientsnumber">
+          <a href="">200+</a>
+          <a href="">100+</a>
+          <a href="">99K+</a>
+          <a href="">24H</a>
+        </div>
+        <div className="clients">
+          <p>Clients Worldwide</p>
+          <p>Clients Worldwide</p>
+          <p>Clients Worldwide</p>
+          <p>Clients Worldwide</p>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Animation
+export default Animation;
